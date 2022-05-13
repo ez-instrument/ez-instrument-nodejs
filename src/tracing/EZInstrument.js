a929d1c106e4771e9819ef82a590f32a55c2e3b8
Pro19 <pranshu188@gmail.com>
2022-05-14 02:06:59 +0530

    implemented basic opentelemetry tracing

--- src/tracing/EZInstrument.js
+++ src/tracing/EZInstrument.js
@@ -35,6 +35,7 @@ class EZInstrument {
      */
     constructor(options) {
         console.info(options);
+        this.tracingOptions = options;
     }
 
     /**
@@ -43,7 +44,93 @@ class EZInstrument {
      * @returns {void}
      */
     initTracing() {
+        if(this.tracingOptions.enableTracing === true) {
+            this.setOpenTelemetryTracing();
+        }
+    }
 
+    /**
+     * @private
+     */
+    setOpenTelemetryTracing() {
+        try {
+            const { BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+            const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+            
+            const { OTLPTraceExporter: OTLPTraceExporterHttp } = require('@opentelemetry/exporter-trace-otlp-http');
+            const { OTLPTraceExporter: OTLPTraceExporterGrpc } = require('@opentelemetry/exporter-trace-otlp-grpc');
+            
+            const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+            const { Resource } = require('@opentelemetry/resources');
+
+            const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+            const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+            const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+
+            const { DiagConsoleLogger, DiagLogLevel, diag } = require('@opentelemetry/api');
+            diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.VERBOSE);
+
+            diag.info("EZ-Instrumentation is initializing OpenTelemetry tracing");
+
+            const serviceResources = new Resource({
+                [SemanticResourceAttributes.SERVICE_NAME]: this.tracingOptions.service.name,
+                [SemanticResourceAttributes.SERVICE_NAMESPACE]: this.tracingOptions.service.namespace,
+                [SemanticResourceAttributes.SERVICE_VERSION]: this.tracingOptions.service.version,
+
+                [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: this.tracingOptions.deployment.environment
+            });
+
+            const nodeTraceProvider = new NodeTracerProvider({
+                resource: serviceResources
+            });
+
+            const httpExporter = new OTLPTraceExporterHttp({
+                url: this.tracingOptions.export.url
+            });
+
+            const grpcExporter = new OTLPTraceExporterGrpc({
+                url: this.tracingOptions.export.url
+            });
+
+            let traceExporter = null;
+            if(this.tracingOptions.export.exporterType === "http") {
+                traceExporter = httpExporter;
+            } else if(this.tracingOptions.export.exporterType === "grpc") {
+                traceExporter = grpcExporter;
+            } else {
+                traceExporter = httpExporter;
+            }
+
+            nodeTraceProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+
+            if(this.tracingOptions.export.enableConsoleExporter === true) {
+                nodeTraceProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+            }
+
+            nodeTraceProvider.register();
+
+            const unloadInstrumentation = registerInstrumentations({
+                tracerProvider: nodeTraceProvider,
+                instrumentations: [
+                    new HttpInstrumentation({
+                        enabled: true
+                    }),
+                    new ExpressInstrumentation({
+                        enabled: true
+                    })
+                ]
+            });
+
+            ['SIGINT', 'SIGTERM'].forEach(signal => {
+                process.on(signal, ()=>{
+                    unloadInstrumentation();
+                    nodeTraceProvider.shutdown();
+                });
+            });
+
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
 
