e24a9ce0e659e4aa2b94b7e1d1a6abaf831c6399
Pro19 <pranshu188@gmail.com>
2022-05-14 02:06:00 +0530

    exporting global tracer for manual instrumentation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const { EZInstrument } = require('./tracing/EZInstrument');
 const { EZInstrumentOptions } = require('./tracing/EZInstrumentOptions');
+const { trace } = require('@opentelemetry/api');
+
+/**
+ * Tracer from the OpenTelemetry global tracer provider which lets you creates spans manually.
+ */
+const globalTracer = trace.getTracer();
 
 module.exports = {
     EZInstrument,
-    EZInstrumentOptions
+    EZInstrumentOptions,
+    globalTracer
 }
