0889fb629dc74d9fe66ca19589b04f403564768a
Pro19 <pranshu188@gmail.com>
2022-05-21 18:01:34 +0530

    added support for nesting spans

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const { EZInstrument } = require('./tracing/EZInstrument');
 const { EZInstrumentOptions } = require('./tracing/EZInstrumentOptions');
-const { trace } = require('@opentelemetry/api');
+const { trace, context, SpanStatusCode } = require('@opentelemetry/api');
+const { Span } = require('@opentelemetry/sdk-trace-base');
 
 /**
  * Tracer from the OpenTelemetry global tracer provider which lets you creates spans manually.
  */
 const globalTracer = trace.getTracer();
 
+/**
+ * Returns span context for nesting spans.
+ * @param {Span} span
+ */
+function getSpanContext(span) {
+    const ctx = trace.setSpan(context.active(), span);
+    return ctx;
+};
+
 module.exports = {
     EZInstrument,
     EZInstrumentOptions,
-    globalTracer
+    globalTracer,
+    getSpanContext,
+    SpanStatusCode
 }
